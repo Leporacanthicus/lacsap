79140dca7466f294a353b81efd2d9face4406cc8
Mats Petersson <mats@planetcatfish.com>
2014-04-11 09:29:26 +0100

    Make test try to detect when it's used wrong

--- test/cfuncname.pas
+++ test/cfuncname.pas
@@ -2,12 +2,17 @@ program cfuncname;
 
 (* Test to ensure we don't have name collisions with C function names *)
 
+var x : integer;
+
 (* Use an arbitrary C function name to do something completely different in Pascal *)
-function strcpy(x : integer; y : integer) : integer;
+function strcpy(var x : integer; y : integer) : integer;
 begin
+   if x <> 3 then
+      writeln('Huh?');
    strcpy := x * y
 end; { strcpy }
 
 begin
-   writeln(strcpy(3,4):1)
+   x := 3;
+   writeln(strcpy(x,4):1)
 end.

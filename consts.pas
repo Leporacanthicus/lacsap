d0c148ebbc78a9f76c8acec9c1e943e68bbd6907
Mats Petersson <mats@planetcatfish.com>
2014-09-18 21:26:26 +0100

    Prepare for bug-fix of shorter strings.

--- consts.pas
+++ consts.pas
@@ -4,11 +4,12 @@ const
    a  = 42;
    b  = a;
    c  = -42;
+   d  = -a;
    aa = 'a';
    bb = true;
    cc = not bb;
       
 begin
-   writeln('a=', a:3, ' b=', b:3, ' c=', c:3, ' bb=', bb, ' cc=', cc);
+   writeln('a=', a:3, ' b=', b:3, ' c=', c:3, ' d=', d:3, ' bb=', bb, ' cc=', cc);
    writeln('aa=', aa:3);
 end.

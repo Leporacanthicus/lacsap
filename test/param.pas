0bc07c3a2ea9bba959bea32a123306589b7e6060
Mats Petersson <mats@planetcatfish.com>
2015-03-22 21:14:42 +0000

    Update param.pas to pass string directly to writeln
    This adds test for bug #4.

--- test/param.pas
+++ test/param.pas
@@ -2,11 +2,9 @@ program param;
 
 var
    i   : integer;
-   str : string;
 
 begin
    for i := 0 to ParamCount do begin
-      str := paramstr(i);
-      writeln("Param ", i:1, "='", str, "'");
+      writeln("Param ", i:1, "='", paramstr(i), "'");
    end;
 end.

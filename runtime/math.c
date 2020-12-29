7b83b28fba7a91fcb2ee7a91f5434b448f766efb
Mats Petersson <mats@planetcatfiish.com>
2020-12-29 22:38:27 +0000

    Implement our own random generator
    This is probably a lower grade random number generator than the one
    from the C runtime library, but it has the advantage of being consistent
    regardless of what platform the compiler is being run on.:w

--- runtime/math.c
+++ runtime/math.c
@@ -1,14 +1,29 @@
 #include <math.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "runtime.h"
 
+
+/* Use our own random number gemerator, so that it is consistent regardless
+ * of what host system is used. Using linear congruent generator.
+ */
+static unsigned rand_seed = 12341193U;
+
+const static unsigned rand_mul = 1103515245U;
+const static unsigned rand_add = 12345;
+
+static unsigned urand()
+{
+    rand_seed = rand_mul * rand_seed + rand_add;
+    return rand_seed;
+}
 /*******************************************
  * Math and such
  *******************************************
  */
 double __random(void)
 {
-    return rand() / (double)RAND_MAX;
+    return urand() / (double)UINT_MAX;;
 }
 
 double __arctan2(double x, double y)

Leporacanthicus/lacsap

commit 0fc91a5f4ad215ba46af9ec8cab7a4a22f46de71

Support array of char as initializers for arrays.
This is an ISO10206 feature missed when implementing array initializers,
which supported strings, but not char arrays.

expr.cpp
@@ -320,8 +320,7 @@ static llvm::Value* TempStringFromCharArray(llvm::Value* dest, AddressableAST* r
     auto func = [](llvm::Value* v, llvm::Value* chars, size_t size)
     {
 	llvm::Type*  charTy = Types::Get<Types::CharDecl>()->LlvmType();
-	llvm::Value* src = builder.CreateGEP(charTy, v, { MakeIntegerConstant(0), MakeIntegerConstant(0) },
-	                                     "src");
+	llvm::Value* src = builder.CreateGEP(charTy, v, { MakeIntegerConstant(0) }, "src");
 	llvm::Align  destAlign{ std::max(AlignOfType(chars->getType()), MIN_ALIGN) };
 	llvm::Align  srcAlign{ std::max(AlignOfType(v->getType()), MIN_ALIGN) };
 	return builder.CreateMemCpy(chars, destAlign, src, srcAlign, size + 1);
@@ -2225,7 +2224,7 @@ llvm::Value* StringExprAST::CodeGen()
 
     BasicDebugInfo(this);
 
-    return builder.CreateGlobalStringPtr(val, "_string", 0, theModule);
+    return builder.CreateGlobalString(val, "_string", 0, theModule);
 }
 
 llvm::Value* StringExprAST::Address()
@@ -3141,6 +3140,7 @@ llvm::Value* VarDeclAST::CodeGenGlobal(VarDef var)
     else if (ExprAST* iv = var.Init())
     {
 	init = llvm::dyn_cast<llvm::Constant>(iv->CodeGen());
+	ICE_IF(!init, "Expected intializer to be constant");
     }
     else
     {
@@ -4228,6 +4228,29 @@ void TrampolineAST::accept(ASTVisitor& v)
     v.visit(this);
 }
 
+static llvm::Constant* MakeCharArray(Types::TypeDecl* type, const ExprAST* value)
+{
+    auto se = llvm::dyn_cast<StringExprAST>(value);
+    assert(se && "£xpect this to be a string");
+    const std::string& val = se->Str();
+
+    auto        arrTy = llvm::dyn_cast<llvm::ArrayType>(type->LlvmType());
+    llvm::Type* eltTy = arrTy->getElementType();
+    size_t      size = type->Size();
+
+    std::vector<llvm::Constant*> initArr(size);
+    for (size_t i = 0; i < val.size(); i++)
+    {
+	initArr[i] = llvm::ConstantInt::get(eltTy, val[i]);
+    }
+    for (size_t i = val.size(); i < size; i++)
+    {
+	initArr[i] = llvm::ConstantInt::get(eltTy, ' ');
+    }
+
+    return llvm::ConstantArray::get(arrTy, llvm::ArrayRef<llvm::Constant*>(initArr));
+}
+
 llvm::Value* InitValueAST::CodeGen()
 {
     if (auto set = llvm::dyn_cast<SetExprAST>(values[0]))
@@ -4249,6 +4272,11 @@ llvm::Value* InitValueAST::CodeGen()
 	}
 
 	return llvm::ConstantDataArray::getRaw(str, size, sd->SubType()->LlvmType());
+    }
+    if (IsCharArray(type))
+    {
+	llvm::Constant* init = MakeCharArray(type, values[0]);
+	return init;
     }
     if (IsStringLike(type) && llvm::isa<Types::ArrayDecl>(type))
     {

test/Basic/arrayinit.pas
@@ -1,0 +1,18 @@
+program arrayinit;
+
+type
+   Elements = (H, He, Li, Be, B, C, N, O, F);
+   ElemNameType = ARRAY [Elements] OF ARRAY [1..10] OF CHAR;
+
+const
+   ElemNames = ElemNameType [ H: 'Hydrogen', He: 'Helium', Li: 'Lithium',
+			     Be: 'Beryllium', B: 'Boron', C:'Carbon',
+			     N: 'Nitrogen', O: 'Oxygen', F:'Fluorine' ];
+
+var
+   e : elements;
+
+begin
+   for e := H to F do
+      writeln(ElemNames[e]);
+end.

test/expected/Basic/arrayinit.tpl
@@ -1,0 +1,9 @@
+Hydrogen  
+Helium    
+Lithium   
+Beryllium 
+Boron     
+Carbon    
+Nitrogen  
+Oxygen    
+Fluorine  

test/testrunner.cpp
@@ -468,6 +468,7 @@ TestEntry testCaseList[] = {
     { 0, "Basic", "Schema Range", "schema.pas", "" },
     { 0, "Basic", "String Slice", "strslice.pas", "" },
     { 0, "Basic", "Compare Array", "comparr.pas", "" },
+    { 0, "Basic", "Array Init", "arrayinit.pas", "" },
 
     { 0, "File", "CopyFile", "copyfile.pas", "File/infile.dat File/outfile.dat" },
     // get from files not supported.

commit 5b2512665405406357696337770af294ea353b34

Fix initializing character arrays with single character
One of the tests in the iso10206-tests suite hit an assert. Fix it by
special-casing single character initialisation of arrays.

This bug was introduced in commit 0fc91a5f, Jul 6th 2024.

Adding a new test to avoid regressing this again.

expr.cpp
@@ -4252,9 +4252,18 @@ void TrampolineAST::accept(ASTVisitor& v)
 
 static llvm::Constant* MakeCharArray(Types::TypeDecl* type, const ExprAST* value)
 {
-    auto se = llvm::dyn_cast<StringExprAST>(value);
-    assert(se && "£xpect this to be a string");
-    const std::string& val = se->Str();
+    std::string val;
+    if (llvm::isa<Types::CharDecl>(value->Type()))
+    {
+	int v = llvm::cast<IntegerExprAST>(value)->Int();
+	val.resize(1, v);
+    }
+    else
+    {
+	auto se = llvm::dyn_cast<StringExprAST>(value);
+	assert(se && "Expect this to be a string");
+	val = se->Str();
+    }
 
     auto        arrTy = llvm::dyn_cast<llvm::ArrayType>(type->LlvmType());
     llvm::Type* eltTy = arrTy->getElementType();

expr.h
@@ -162,7 +162,7 @@ public:
     }
     void         DoDump() const override;
     llvm::Value* CodeGen() override;
-    uint64_t     Int() { return val; }
+    uint64_t     Int() const { return val; }
     static bool  classof(const ExprAST* e)
     {
 	return e->getKind() == EK_IntegerExpr || e->getKind() == EK_CharExpr;

test/Basic/inittype.pas
@@ -8,6 +8,7 @@ type
    aa = array [1..20] of real;
    bb = aa[1..10: 7.125 otherwise 9.5];
    cc = array [1..10] of aa;
+   dd = array [1..3] of char value '*';
 
 const
    init	 = aa[1..20: 2.375];
@@ -15,14 +16,15 @@ const
    init3 = cc[1..4: init2 otherwise init];
    
 var
-   n : t;
-   m : r;
-   x : r value 18.7;
-   a : c;
-   b : c value '#';
-   v : e;
-   w : e value red;
+   n  : t;
+   m  : r;
+   x  : r value 18.7;
+   a  : c;
+   b  : c value '#';
+   v  : e;
+   w  : e value red;
    xx : bb;
+   yy : dd;
    
 begin
    writeln("n=", n, " m=", m, " x=", x);
@@ -32,5 +34,6 @@ begin
    writeln("xx[1]=", xx[1]:8:4, " xx[10]=", xx[10]:8:4, " xx[11]=", xx[11]:8:4, " xx[19]=", xx[19]:8:4,
 	   " xx[20]=", xx[20]:8:4);
    writeln("init3[1][1]=", init3[1][1]:8:4," init3[2][2]=", init3[2][2]:8:4," init3[4][6]=", init3[4][6]:8:4,
-	   " init3[5][1]=", init3[5][1]:8:4," init3[10][17]=", init3[10][17]:8:4); 
+	   " init3[5][1]=", init3[5][1]:8:4," init3[10][17]=", init3[10][17]:8:4);
+   writeln("yy=", yy);
 end.

test/expected/Basic/inittype.tpl
@@ -4,3 +4,4 @@ v=1 w=2
 Good
 xx[1]=  7.1250 xx[10]=  7.1250 xx[11]=  9.5000 xx[19]=  9.5000 xx[20]=  9.5000
 init3[1][1]=  2.5000 init3[2][2]=  2.5000 init3[4][6]=  5.0000 init3[5][1]=  2.3750 init3[10][17]=  2.3750
+yy=*  